cad936831ff4bf2cbc218cab7a099783df4a3914
ivandgc <ivandgc@hotmail.com>
2017-10-03 16:50:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  let indexOfMin = 0
+  let min = array[0]
+  for (var i = 1; i < array.length; i++) {
+    if(array[i] < min){
+      min = array[i]
+      indexOfMin = i
+    }
+  }
+  return array.splice(indexOfMin, 1)[0]
 }
 
 function insertionSort(array){
-
+  let newArr = []
+  while(array.length != 0){
+    newArr.push(findMinAndRemove(array))
+  }
+  return newArr
 }
