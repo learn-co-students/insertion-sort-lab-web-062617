133314cecced2b6c7ef6235641bdd6de362776ab
wk645 <wk645@nyu.edu>
2017-10-03 16:38:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
+	let min = array[0]
+	let minIndex = 0
 
+	for (let i = 0; i < array.length; i++) {
+		let currentElement = array[i]
+		if (array[i] < min) {
+			min = array[i]
+			minIndex = i
+		}
+	}
+	array.splice(minIndex, 1)
+	// removes one item at the minIndex position
+	return min
 }
 
 function insertionSort(array){
+	let finalArray = []
+	let newMin
 
+	while (array.length != 0) {
+		newMin = findMinAndRemove(array)
+		finalArray.push(newMin)
+	}
+	
+	return finalArray
 }
